8281ebad72ff20db25a1e37e2b2ad702c3d17e1a
Dant86 <vedantdpathak@iCloud.com>
2020-06-25 14:00:24 +0100

    flask run command now works

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
+from sqlalchemy_utils import database_exists, create_database
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
-from models import Base
+from .models import Base
 from flask import Flask
 from os import getenv
 
@@ -12,9 +13,11 @@ db_pass = getenv('DB_PASS')
 db_host = getenv('DB_HOST')
 db_name = getenv('DB_NAME')
 
-mysql_url = f'mysql://{db_usr}:{db_pass}@{db_host}/{db_name}'
+mysql_url = f'mysql://{db_usr}:{db_pass}@{db_host}/{db_name}?ssl=false'
 
 engine = create_engine(mysql_url)
+if not database_exists(engine.url):
+    create_database(engine.url)
 Base.metadata.create_all(engine, checkfirst=True)
 Session = sessionmaker(bind=engine)
 session = Session()
