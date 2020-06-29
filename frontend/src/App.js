f24ebb2833f557798362cf2137151c7b82d28108
Will Cravitz <willcravitz@Wills-MacBook-Pro.local>
2020-06-28 23:03:17 -0500

    added react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <h1>Fanbase Market</h1>
     </div>
   );
 }
