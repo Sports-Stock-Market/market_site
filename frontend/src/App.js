7ebd5355dd037ea33b4b40a086d886d545d13cfc
Will Cravitz <willcravitz@Wills-MacBook-Pro.local>
2020-06-29 19:06:45 -0500

    bootstrapped login and register components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,14 +4,44 @@ import {
   Switch,
   Route,
   Link
-} from "react-router-dom";
+} from 'react-router-dom';
+
+import {
+  Login,
+  Register
+} from './components';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Fanbase Market</h1>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/" exact>
+            <h1> Fanbase Market </h1>
+          </Route>
+          <Route path="/login" exact component={Login} />
+          <Route path="/register" exact component={Register} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
