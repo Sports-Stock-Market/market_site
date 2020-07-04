3a02c1d701fa5a2dd1c180cd0debb1a3309590a4
Dant86 <vedantdpathak@iCloud.com>
2020-07-04 12:19:05 +0100

    get_schedule_range makes life a little easier now

--- fanbasemarket/pricing/utils.py
+++ fanbasemarket/pricing/utils.py
@@ -2,5 +2,11 @@ from basketball_reference_web_scraper import client
 
 
 def get_schedule_range(start_yr, end_yr):
-    return [client.season_schedule(season_end_year=yr) for
-            yr in range(start_yr+1, end_yr+1)]
+    schedules = []
+    for year in range(start_yr + 1, end_yr + 1):
+        try:
+            schedule = client.season_schedule(season_end_year=year)
+            schedules.append(schedule)
+        except:
+            pass
+    return schedules
