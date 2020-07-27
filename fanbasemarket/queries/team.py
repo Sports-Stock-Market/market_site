913a82c38788272a13f689331c6b35164ff44ca2
Dant86 <vedantdpathak@iCloud.com>
2020-07-27 18:27:47 +0100

    user position done

--- fanbasemarket/queries/team.py
+++ fanbasemarket/queries/team.py
@@ -3,8 +3,9 @@ from datetime import datetime
 from sqlalchemy import desc
 
 from sqlalchemy.ext.declarative import declarative_base
-from fanbasemarket.models import Teamprice, Player
+from fanbasemarket.models import Teamprice, Player, Purchase
 from fanbasemarket.queries.utils import get_graph_x_values
+from fanbasemarket.queries.user import get_active_holdings
 
 def get_price(tid, db, date=None):
     if not date:
@@ -59,3 +60,25 @@ def active_player_rating(team, db):
         filter(Player.is_injured == False).\
             all()
     return sum([player.rating * player.mpg for player in active_ps])
+
+def get_user_position(team, user, db):
+    holdings = Purchase.query.\
+        filter(Purchase.user_id == user.id).\
+        filter(Purchase.team_id == team.id).\
+        filter(Purchase.exists == True).\
+        all()
+    values = [h.purchased_for for h in holdings]
+    bought_at = sum(values) / len(values)
+    num_shares = sum([h.amt_shares for h in holdings])
+    all_holdings = get_active_holdings(user.id, db)
+    total_val = 0
+    for h in all_holdings:
+        for abr, item in h.items():
+            tm = Team.query.filter(Team.abr == abr).first()
+            total_val += tm.price
+    weight = team.price * num_shares / total_val
+    d = {}
+    d['bought_at'] = bought_at
+    d['num_shares'] = num_shares
+    d['weight'] = weight
+    return d
